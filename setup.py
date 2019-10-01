279b7987f1ccb17b0cca73361d2026e18bb01f48
John Internet <jonjon1234.github@gmail.com>
2019-10-01 17:37:18 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='InterWorkshop',
+    version='1.0.0',
+    packages=['gmad', 'workshop'],
+    url='doctor-internet.dev',
+    license='MIT',
+    author='John Internet',
+    author_email='jonjon1234.github@gmail.com',
+    description='A Python binding for the Steam Workshop API'
+)
