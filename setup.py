0177a79f65336863d2b4d9126413880ecb0ffd1f
John Internet <jonjon1234.github@gmail.com>
2019-10-01 17:50:40 +0100

    Changed version to dev1.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='InterWorkshop',
-    version='1.0.0',
+    version='1.0.0.dev1',
     packages=['gmad', 'workshop'],
     url='doctor-internet.dev',
     license='MIT',
