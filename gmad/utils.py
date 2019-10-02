d1e85a4c1161ff1c0f736379167028df6d01b29c
John Internet <jonjon1234.github@gmail.com>
2019-10-02 14:50:35 +0100

    Reformat.

--- gmad/utils.py
+++ gmad/utils.py
@@ -1,8 +1,13 @@
 import chardet
 
 """Various utilities for stuff."""
+
+
 def read_until_null(file):
-    """Read a file pointer until it reaches a null byte, returning an array of byte objects."""
+    """
+    Read a file pointer until it reaches a null byte,
+    returning an array of byte objects.
+    """
     return_data = b""
     next_byte = file.read(1)
     while next_byte != b"\x00":
@@ -10,8 +15,12 @@ def read_until_null(file):
         next_byte = file.read(1)
     return return_data
 
+
 def read_null_string(file):
-    """Read a null terminated string from a filer pointer, returning the read string."""
+    """
+    Read a null terminated string from a filer pointer,
+    returning the read string.
+    """
     raw = read_until_null(file)
     cset = chardet.detect(raw)['encoding']
     if cset is None:
@@ -20,16 +29,23 @@ def read_null_string(file):
 
 
 def read_length_string(file, length=0):
-    """Return a string of given length pulled from the file."""
+    """
+    Return a string of given length pulled from the file.
+    """
     raw = file.read(length)
     cset = chardet.detect(raw)['encoding']
     if cset is None:
         cset = "ascii"
     return raw.decode(cset)
 
+
 def read_int(file, length=4, edian='little'):
-    """Read integer from file, returning an int of byte length and edian encoding."""
+    """
+    Read integer from file,
+    returning an int of byte length and edian encoding.
+    """
     return int.from_bytes(file.read(length), edian)
+
 
 def list_has_item(in_list, item):
     """Return if the list has a given item."""
