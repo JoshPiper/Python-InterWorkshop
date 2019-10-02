fc4c46320d1cfc02e6dfe437e99b2f92605fcc90
John Internet <jonjon1234.github@gmail.com>
2019-10-02 18:35:58 +0100

    Fixed args not being tuples, added bans, summaries and publisherappownership.

--- steam_api/ISteamUser.py
+++ steam_api/ISteamUser.py
@@ -4,24 +4,45 @@ from . import BaseSteamAPIService
 class ISteamUserAPI(BaseSteamAPIService):
     autoMethods = {
         'CheckAppOwnership': {
-            'args': {
+            'args': (
                 (0, 'steamid', True),
-                (1, 'appid', True)
-            }
+                (1, 'appid', True),
+            )
         },
         'GetAppPriceInfo': {
-            'args': {
+            'args': (
                 (0, 'steamid', True),
-                (1, 'appids', True)
-            },
+                (1, 'appids', True),
+            ),
             'version': 'v1'
         },
         'GetFriendList': {
-            'args': {
-                (0, 'steamid', True),
-                (1, 'relationship', False)
-            },
-            'version': 'v1'
+            'args': (
+                ('steamid', True),
+                ('relationship', False, None),
+            ),
+            'version': 'v1',
+            'datakey': ('friendslist', 'friends')
+        },
+        'GetPlayerBans': {
+            'args': (
+                (0, 'steamids', True),
+            ),
+            'version': 'v1',
+            'datakey': 'players'
+        },
+        'GetPlayerSummaries': {
+            'args': (
+                (0, 'steamids', True),
+            ),
+            'datakey': ('response', 'players')
+        },
+        'GetPublisherAppOwnership': {
+            'args': (
+                ('steamid', True),
+            ),
+            'version': 'v3'
         }
+
     }
 
