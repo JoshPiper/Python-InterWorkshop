16bc14d4dd595ec688af180d4cd8056124b38405
John Internet <jonjon1234.github@gmail.com>
2019-10-02 10:17:26 +0100

    Added install requirements to setup.

--- setup.py
+++ setup.py
@@ -8,5 +8,18 @@ setup(
     license='MIT',
     author='John Internet',
     author_email='jonjon1234.github@gmail.com',
-    description='A Python binding for the Steam Workshop API'
+    description='A Python binding for the Steam Workshop API',
+    install_requires=[
+        'certifi==2019.6.16',
+        'chardet==3.0.4',
+        'discord-webhook==0.4.1',
+        'environs==5.2.1',
+        'idna==2.8',
+        'marshmallow==2.20.1',
+        'protobuf==3.9.1',
+        'python-dotenv==0.10.3',
+        'requests==2.22.0',
+        'six==1.12.0',
+        'urllib3==1.25.3'
+    ]
 )
